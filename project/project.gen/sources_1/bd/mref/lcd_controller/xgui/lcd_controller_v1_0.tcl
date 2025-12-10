e18d48d28aa26c895c8d8e415bc896e61d276569
Hoang Kiet <nhoangkiet35@gmail.com>
2025-12-10 13:56:36 +0700

    gen

--- project/project.gen/sources_1/bd/mref/lcd_controller/xgui/lcd_controller_v1_0.tcl
+++ project/project.gen/sources_1/bd/mref/lcd_controller/xgui/lcd_controller_v1_0.tcl
@@ -2,9 +2,39 @@
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
-  ipgui::add_page $IPINST -name "Page 0"
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "CLK_FREQ_HZ" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "LCD_I2C_ADDR" -parent ${Page_0}
 
 
 }
 
+proc update_PARAM_VALUE.CLK_FREQ_HZ { PARAM_VALUE.CLK_FREQ_HZ } {
+	# Procedure called to update CLK_FREQ_HZ when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.CLK_FREQ_HZ { PARAM_VALUE.CLK_FREQ_HZ } {
+	# Procedure called to validate CLK_FREQ_HZ
+	return true
+}
+
+proc update_PARAM_VALUE.LCD_I2C_ADDR { PARAM_VALUE.LCD_I2C_ADDR } {
+	# Procedure called to update LCD_I2C_ADDR when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.LCD_I2C_ADDR { PARAM_VALUE.LCD_I2C_ADDR } {
+	# Procedure called to validate LCD_I2C_ADDR
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.CLK_FREQ_HZ { MODELPARAM_VALUE.CLK_FREQ_HZ PARAM_VALUE.CLK_FREQ_HZ } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.CLK_FREQ_HZ}] ${MODELPARAM_VALUE.CLK_FREQ_HZ}
+}
+
+proc update_MODELPARAM_VALUE.LCD_I2C_ADDR { MODELPARAM_VALUE.LCD_I2C_ADDR PARAM_VALUE.LCD_I2C_ADDR } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.LCD_I2C_ADDR}] ${MODELPARAM_VALUE.LCD_I2C_ADDR}
+}
 
